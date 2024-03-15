6dec01dfed967d36ca6c885fef41cf514918944b
Thang Macbook <duongnhatthang@gmail.com>
2024-03-15 16:27:24 -0700

    update data processing code

--- data/data_processing.py
+++ data/data_processing.py
@@ -2,6 +2,8 @@ import glob
 import pandas as pd
 import numpy as np
 
+from tqdm import tqdm
+
 import matplotlib.pyplot as plt
 import sys
 import pydicom
@@ -9,8 +11,8 @@ from pydicom.data import get_testdata_files
 
 selected_wave = "BL" #BL, M12, M24, M36
 
-def get_paths():
-    path_list = glob.glob("/Volumes/Elements/MOAKS/*")
+def save_paths_to_csv(input_path):
+    path_list = glob.glob(input_path)
     names = [p.split("/")[-1] for p in path_list]
     # print(len(names))
     # print(names[:10])
@@ -25,11 +27,12 @@ def get_paths():
     id_list = list(id)
     knee_list = list(knee)
     es_list = list(es)
+    study_list = list(study)
     label_names = []
     out_path = []
     out_labels = []
     for i, id in enumerate(id_list):
-        if wave[i] != selected_wave:
+        if not isinstance(study_list[i], str) or wave[i] != selected_wave:
             continue
         name = str(id)+str(knee_list[i])
         label_names.append(name)
@@ -85,30 +88,38 @@ def read_dicom(path):
     # plt.imshow(dataset.pixel_array, cmap=plt.cm.bone)
     # plt.show()
 
+def processing_data_from_csv_paths(csv_path):
+    df = pd.read_csv(csv_path)
+    path_list = list(df['path'])
+    es_list = list(df['es'])
 
-df = pd.read_csv("paths_and_labels.csv")
-path_list = list(df['path'])
-es_list = list(df['es'])
+    min_num_slices = 59
+    num_continuous_slices = 20
+    cache = np.zeros((len(path_list), min_num_slices, 384, 384))
+    for i, p in enumerate(tqdm(path_list)):
+        slice_path_list = glob.glob(p+'/'+selected_wave+'/AXLD'+'/*')
 
-# min_value = 200
+        for j, slice_path in enumerate(slice_path_list):
+        #     read_dicom(slice_path)
+        #     break
+            if j>=min_num_slices:
+                break
+            s = pydicom.dcmread(slice_path)
+            assert 'PixelData' in s, "PixelData not in s"
+            # print(np.max(s.pixel_array), np.min(s.pixel_array), np.shape(s.pixel_array))
+            # break
+            cache[i,j] = s.pixel_array
+    out = np.sum(cache, axis=-1)
+    out = np.sum(out, axis=-1) #(N, min_num_slices)
+    out = np.sum(out, axis=0) #(min_num_slices)
+    min = np.sum(out)
+    index = -1
+    for i in tqdm(range(min_num_slices-num_continuous_slices)):
+        cur_sum = np.sum(out[i:i+num_continuous_slices])
+        if cur_sum < min:
+            min = cur_sum
+            index = i
+    print(f"Choosing slides: {index}:{index+num_continuous_slices}")
 
-for p in path_list:
-    slice_path_list = glob.glob(p+'/'+selected_wave+'/AXLD'+'/*')
-    min_value = min(min_value, len(slice_path_list))
-
-    # count = 0
-    # for slice_path in slice_path_list:
-    #     read_dicom(slice_path)
-    #     break
-
-    #     s = pydicom.dcmread(slice_path)
-    #     loc = s.get('SliceLocation')
-    #     if loc is not None:
-    #         count+=1
-    #         print(loc)
-    # if count > 0:
-    #     print(f"{p}, count = {count}")
-    #     count = 0
-
-    # break
-print(f"min_value={min_value}")
+# save_paths_to_csv("/Volumes/Elements/MOAKS/*")
+processing_data_from_csv_paths("paths_and_labels.csv")
